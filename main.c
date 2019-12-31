c14a25b7dec0f160d3fb20afd371d20cbf9ff1bd
CodeRed23 <defrann8208@outlook.com>
2019-12-31 14:40:50 -0500

    hid incomplete code with #ifdef and fixed diff chars appearing on board

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 #include <math.h>
 #include <time.h>
 #include <signal.h>
@@ -9,14 +10,17 @@
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/uio.h>
 #include <ncurses.h>
+
+extern int errno;
 
 #define PORT 12345
 
 struct winsize size;
 
-int grid_rows = 7;
-int grid_cols = 20;
+int grid_rows = 4;
+int grid_cols = 7;
 #define GRID_Y                                  4
 #define GRID_X                                  3
 
@@ -50,23 +54,99 @@ static int flags = 0;
 static void clean(void) {
 
 	int i = 0;
-	if(puzzle != NULL) {
-		for(i = (size.ws_row - GRID_Y) / 2; i >= 0; i--) {
-			free(puzzle[i]);
-			puzzle[i] = NULL;
+	int j = 0;
+	for(i = 0; i < grid_rows; i++) {
+		for(j = 0; j < grid_cols; j++) {
 			free(playboard[i]);
-			playboard[i] = NULL;
+			free(puzzle[i]);
 		}
 	}
 
 	free(puzzle);
-	puzzle = NULL;
 	free(playboard);
 	playboard = NULL;
 
 	endwin();
 
 }
+
+#ifdef DEBUG
+
+static int backup(size_t no, ...) 
+{
+	int fd = creat("backup", S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	if(fd == -1) {
+		/* errors */	
+		return errno;
+	}
+
+	va_list ap;
+	va_start(ap, no);
+	struct iovec *iov = alloca(no);
+	size_t i;
+	for(i = 0; i < no; i++) {
+		iov[i].iov_base = va_arg(ap, char *);
+		iov[i].iov_len = grid_rows * grid_cols;
+	}
+
+	iov[no].iov_base = &grid_rows;
+	iov[no].iov_len = sizeof(int);
+
+	iov[no + 1].iov_base = &grid_cols;
+	iov[no + 1].iov_len = sizeof(int);
+
+	if(writev(fd, iov, no) < 0) {
+		return errno;
+	}
+
+	va_end(ap);
+	close(fd);
+	
+	return EXIT_SUCCESS;
+}
+
+static int retrieve(size_t no, ...) 
+{
+	int fd = open("backup", O_RDWR | O_NONBLOCK);
+	struct stat sb;
+	if(fstat(fd, &sb)) {
+		return errno;
+	}
+
+	if(fd < 0) {
+		return errno;
+	}
+
+	posix_fadvise(fd, 0, 0, POSIX_FADV_SEQUENTIAL);
+
+	va_list ap;
+	va_start(ap, no);
+	struct iovec *iov = alloca(no + 2);
+	size_t i;
+	for(i = 0; i < no; i++) {
+		iov[i].iov_base = va_arg(ap, char *);
+		iov[i].iov_len = grid_rows * grid_cols;
+	}
+
+	iov[no].iov_base = &grid_rows;
+	iov[no].iov_len = sizeof(int);
+
+	iov[no + 1].iov_base = &grid_cols;
+	iov[no + 1].iov_len = sizeof(int);
+
+	if(readv(fd, iov, no) < 0) {
+		return errno;
+	}
+
+	va_end(ap);
+	if(close(fd) < 0) {
+		return errno;
+	}
+
+	return EXIT_SUCCESS;
+}
+
+#endif
 
 // sets up the interface for the game
 // used also when terminal is resized
@@ -103,16 +183,14 @@ static int init(void)
 }
 
 // 
-static void create_puzzle(void) {
+static void create_puzzle(void) 
+{
 
 	int i = 0;
 	int j = 0;
 	int count = 0;
 
 	bombs = (grid_rows * grid_cols) / 6;
-
-	wprintw(status, "bomb(s) = %d, flag(s) = %d", bombs, flags);
-	wrefresh(status);
 
 	for(i = 0; i < grid_rows; i++) {
 		for(j = 0; j < grid_cols; j++) {
@@ -174,9 +252,7 @@ static void draw_grid(void)
 				waddch(grid, ACS_PLUS);
 
 			if(j < grid_cols)
-			{
 				waddch(grid, ACS_HLINE);
-			}
 		}
 		waddch(grid, '\n');
 		for(j = 0;j <= grid_cols && i < grid_rows;j++) {
@@ -195,7 +271,8 @@ static void draw_grid(void)
 
 }
 
-static void reset(void) {
+static void reset(void) 
+{
 
 	wrefresh(status);
 	getch();
@@ -215,7 +292,7 @@ static void reset(void) {
 
 }
 
-
+#ifdef DEBUG
 static void resize_game(void) {
 
 	int tmp_rows = 0, tmp_cols = 0;
@@ -271,6 +348,7 @@ static void resize_ter(void) {
 
 }
 
+#endif
 
 int main(int argc, char **argv) {
 
@@ -285,6 +363,10 @@ int main(int argc, char **argv) {
 	}
 
 	create_puzzle();
+
+	wprintw(status, "bomb(s) = %d, flag(s) = %d", bombs, flags);
+	wrefresh(status);
+
 	draw_grid();
 	wmove(grid, y, x);	
 	wrefresh(grid);
@@ -316,9 +398,11 @@ int main(int argc, char **argv) {
 			case 'q':
 			run = false;
 			break;
+			#ifdef DEBUG
 			case 'r':
 			resize_game();
 			break;
+			#endif
 			case 'f':
 			if(OFF(playboard, x, y) == ' ') { 
 				(OFF(playboard, x, y)) = 'f';
@@ -351,9 +435,34 @@ int main(int argc, char **argv) {
 				}
 			}
 			break;
+			#ifdef DEBUG
 			case KEY_RESIZE:
 			resize_ter();
 			break;
+			case 's':
+			if((backup(2, puzzle, playboard)) >= 0) {
+				werase(status);
+				wprintw(status, "successfully created backup");
+				wrefresh(status);
+			} else {
+				werase(status);
+				wprintw(status, "Failed to create backup");
+				wrefresh(status);
+			}
+			break;
+			case 'b':
+			if(retrieve(2, puzzle, playboard) >= 0) {
+				werase(status);
+				wprintw(status, "successfully retrieved backup");
+				draw_grid();
+				wrefresh(status);
+			} else {
+				werase(status);
+				wprintw(status, "Failed to create backup");
+				wrefresh(status);
+			}
+			break;
+			#endif
 		}
 
 		wrefresh(status);
@@ -361,6 +470,6 @@ int main(int argc, char **argv) {
 		wrefresh(grid);
 	}
 
-	return 0;
+	return EXIT_SUCCESS;
 }
 
